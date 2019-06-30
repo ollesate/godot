191b2a1e6ffc020d23f8275f8db8c1b5c9d6f284
Olof Sjöholm <olof.sjoholm@walmartlabs.com>
2019-06-30 21:22:20 +0200

    dasd

--- TileMap.gd
+++ TileMap.gd
@@ -57,7 +57,6 @@ func create(name, cellPos, flipX, flipY, transposed):
 		"Turn1":
 			var belt = preload("res://conveyor.tscn").instance()
 			belt.steps = 1
-			belt.rotate = -90
 			belt.direction = Vector2(-1, 0)
 			belt.get_node("Sprite").region_enabled = true
 			belt.get_node("Sprite").region_rect = getRect(0, 1)
@@ -65,12 +64,16 @@ func create(name, cellPos, flipX, flipY, transposed):
 			if transposed:
 				belt.direction = Vector2(belt.direction.y, belt.direction.x)
 			belt.direction = Vector2(belt.direction.x * -1 if flipX else belt.direction.x, belt.direction.y * -1 if flipY else belt.direction.y)
+			var up = Vector2(0, -1)
+			if transposed:
+				up = Vector2(up.y, up.x)
+			up = Vector2(up.x * -1 if flipX else up.x, up.y * -1 if flipY else up.y)
+			belt.rotate = rad2deg(up.angle_to(belt.direction))
 			belts[key] = belt
 			return belt
 		"Turn2":
 			var belt = preload("res://conveyor.tscn").instance()
 			belt.steps = 2
-			belt.rotate = 90
 			belt.direction = Vector2(1, 0)
 			belt.get_node("Sprite").region_enabled = true
 			belt.get_node("Sprite").region_rect = getRect(1, 1)
@@ -78,6 +81,11 @@ func create(name, cellPos, flipX, flipY, transposed):
 			if transposed:
 				belt.direction = Vector2(belt.direction.y, belt.direction.x)
 			belt.direction = Vector2(belt.direction.x * -1 if flipX else belt.direction.x, belt.direction.y * -1 if flipY else belt.direction.y)
+			var up = Vector2(0, -1)
+			if transposed:
+				up = Vector2(up.y, up.x)
+			up = Vector2(up.x * -1 if flipX else up.x, up.y * -1 if flipY else up.y)
+			belt.rotate = rad2deg(up.angle_to(belt.direction))
 			belts[key] = belt
 			return belt
 			
@@ -97,8 +105,28 @@ func getBeltAction(step):
 		if belts.has(cellPos):
 			var belt = belts[cellPos]
 			if belt.steps >= step:
+				var next = cellPos + belt.direction
+				if belts.has(next):
+					var nextBelt = belts[next]
+					if nextBelt.rotate != 0:
+						actions.append(RotateAction.new(nextBelt.rotate, 1.0).with(player))
 				actions.append(BeltMove.new(belt).with(player))
 	return Parallell.new(actions)
+
+class RotateAction:
+	extends Action
+	
+	var rotation
+	var duration
+	
+	func _init(rotation, duration):
+		self.rotation = deg2rad(rotation)
+		self.duration = duration
+	
+	func act(delta):
+		duration -= delta
+		character.rotate(rotation * delta)
+		return duration <= 0
 
 class BeltMove:
 	extends ActionMove
@@ -107,28 +135,19 @@ class BeltMove:
 	const STEP_SIZE = 75.0
 	
 	var belt
-	var exited
 	
 	func _init(belt).(belt.direction, DURATION, STEP_SIZE / DURATION):
 		self.belt = belt
 	
 	func start():
 		belt.start()
-		belt.connect("body_entered", self, "bodyEntered")
-		belt.connect("body_exited", self, "bodyExited")
-		character.get_node("CollisionShape2D").disabled = true
+		character.collision_mask = 2 
 	
 	func finish():
 		belt.stop()
-		character.get_node("CollisionShape2D").disabled = false
+		character.collision_mask = 1
 		
-	
 	func act(delta):
-		if .act(delta):
-			# Timed out (haven't moved out of the area)
+		if character.get_slide_count() > 0:
 			return true
-		return false
-	
-	func bodyExited(body):
-		if body == character:
-			exited = true
+		return .act(delta)
