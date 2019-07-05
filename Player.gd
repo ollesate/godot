6934bfc7fd8e42fc48f79c98d920fa79c528cb57
Olof Sjöholm <olof.sjoholm@walmartlabs.com>
2019-07-05 02:24:16 +0200

    WIP

--- Player.gd
+++ Player.gd
@@ -17,14 +17,17 @@ const HP_MAX = 9
 static func moveAction(movement, steps):
 	var actions = []
 	for i in range(steps):
-		actions.append(MoveStep.new(movement))
+		actions.append(MovePlayer.new(movement))
 		actions.append(Actions.wait(1))
-	return Parallell.new(actions)
+	return Sequence.new(actions)
 	
 static func rotateAction(rotation):
 	if rotation == UTURN:
 		return RotateAction.new(rotation, 2)
 	return RotateAction.new(rotation, 1)
+
+static func shootAction():
+	return Shoot.new()
 
 onready var hp = $UI/HP
 onready var nameLabel = $UI/NameLabel
@@ -93,3 +96,20 @@ class MovePlayer:
 			if collider.is_in_group("players"):
 				var player: KinematicBody2D = collider
 				player.move_and_slide(moveAction.velocity)
+
+class Shoot:
+	extends Action
+	
+	var isHit = false
+
+	func start():
+		# Shoot and wait for it to hit something...
+		var bullet = preload("res://Game/Bullet.tscn").instance()
+		bullet.rotation = self.character.rotation
+		bullet.playerOwner = self.character
+		self.character.add_child(bullet)
+		yield(bullet, "onHit")
+		isHit = true
+	
+	func act(delta):
+		return isHit
